4dc6946077daa7f6c286cd97d755345599a77b62
lizhen325 <zhenli336@gmail.com>
2016-02-16 14:33:33 +1300

    283. MoveZeroes 	new file:   .gitattributes 	new file:   LeetCode/LeetCode/LeetCode.sln 	new file:   LeetCode/LeetCode/LeetCode/283_MoveZeroes.cs 	new file:   LeetCode/LeetCode/LeetCode/App.config 	new file:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/LeetCode/Program.cs 	new file:   LeetCode/LeetCode/LeetCode/Properties/AssemblyInfo.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/Properties/AssemblyInfo.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest283_MoveZeroes.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/283_MoveZeroes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class MoveZeroes
+    {
+        public void Move_zeroes(int[] nums)
+        {
+            // Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+            //For example, given nums = [0, 1, 0, 3, 12], after calling your function, nums should be [1, 3, 12, 0, 0]. 
+
+            int current = 0;
+            for(int i=0; i<nums.Length; i++)
+            {
+                if(nums[i] != 0)
+                {
+                    nums[current] = nums[i];
+                    current++;
+                }
+            }
+
+            for(int i=current; i<nums.Length; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest283_MoveZeroes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class UnitTest283_MoveZeroes
+    {
+        [TestMethod()]
+        public void MoveZeroesTest()
+        {
+            int[] arr = { 1, 2, 3, 0, 0, 2, 4, 2, 0, 4 };
+            MoveZeroes mz = new MoveZeroes();
+            mz.Move_zeroes(arr);
+            CollectionAssert.AreEqual(new int[] { 1, 2, 3, 2, 4, 2, 4, 0, 0, 0 },arr);
+        }
+    }
+}

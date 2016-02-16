ba362f047a0a357f5add586dfc581bbd05eaafda
lizhen325 <zhenli336@gmail.com>
2016-02-17 02:53:19 +1300

    268.Missing Number 	new file:   LeetCode/LeetCode/LeetCode/268_MissingNumber.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest268MissingNumber.cs 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/268_MissingNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _268
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            Array.Sort(nums);
+            int counter;
+            for(counter=0; counter<nums.Length; counter++)
+            {
+                if(counter!= nums[counter])
+                {
+                    return counter;
+                }
+            }
+            return counter;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest268MissingNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class UnitTest268MissingNumber
+    {
+        [TestMethod()]
+        public void MissingNumber()
+        {
+            int[] nums = { 0, 1, 3 };
+            int missingNumber = _268.MissingNumber(nums);
+            Assert.AreEqual(2, missingNumber);
+
+            int[] res = { 0 };
+            int ms = _268.MissingNumber(res);
+            Assert.AreEqual(1, ms);
+
+            int[] res2 = { 1 };
+            int ms2 = _268.MissingNumber(res2);
+            Assert.AreEqual(0, ms2);
+        }
+    }
+}

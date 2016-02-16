255c92b3cf847d186156b68f6710124d81199ddf
lizhen325 <zhenli336@gmail.com>
2016-02-17 11:39:41 +1300

    35. Search Insert Position 	new file:   LeetCode/LeetCode/LeetCode/35._SearchInsertPosition.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest35_SearchInsertPosition.cs 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/35._SearchInsertPosition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _35
+    {
+        public static int SearchInsert(int[]nums,int target)
+        {
+            int i;
+            for(i=0; i<nums.Length; i++)
+            {
+                if (nums[i + 1] >= target && nums[i] < target && nums.Length > 1)
+                    return i + 1;
+                else if (nums[i] >= target)
+                    return i;
+                else if (nums[nums.Length - 1] < target)
+                    return nums.Length;
+                else if (nums[i] == target)
+                    return i;
+            }
+            return i;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest35_SearchInsertPosition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class UnitTest35_SearchInsertPosition
+    {
+        [TestMethod()]
+        public void SearchInsertPosition()
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            int position = _35.SearchInsert(nums, 0);
+            Assert.AreEqual(0, position);
+        }
+    }
+}

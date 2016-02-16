47a3091a79126b633f0c5d9d3507afd49d82dca7
lizhen325 <zhenli336@gmail.com>
2016-02-16 15:36:19 +1300

    122_Best_Time__to_Buy_and_Sell_Stock_II 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests/Best_Time_to_Buy_and_Sell_Stock_IITests.csproj 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests/Properties/AssemblyInfo.cs 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests/UnitTest283_MoveZeroes.cs 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests1/Best_Time_to_Buy_and_Sell_Stock_IITests1.csproj 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests1/Properties/AssemblyInfo.cs 	new file:   LeetCode/LeetCode/Best_Time_to_Buy_and_Sell_Stock_IITests1/UnitTest122_Best_Time__to_Buy_and_Sell_Stock_II.cs 	new file:   LeetCode/LeetCode/LeetCode/122_Best_Time _to_Buy_and_Sell_Stock_II.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest122_Best_Time__to_Buy_and_Sell_Stock_II.cs 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/122_Best_Time _to_Buy_and_Sell_Stock_II.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class Best_Time__to_Buy_and_Sell_Stock_II
+    {
+        public int MaxProfit(int[] prices)
+        {
+            int profits = 0;
+            for (int i = 1; i < prices.Length; i++ )
+            {
+                int diff = prices[i] - prices[i - 1];
+                if(diff > 0)
+                {
+                    profits += diff;
+                }
+            }
+                return profits;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest122_Best_Time__to_Buy_and_Sell_Stock_II.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace LeetCLeetCode.Tests
+{
+    [TestClass]
+    public class UnitTest122_Best_Time__to_Buy_and_Sell_Stock_II
+    {
+        [TestMethod]
+        public void MaxProfitTest()
+        {
+            int[] arr = { 1, 2, 3, 4, 5, -2, -5 };
+            Best_Time__to_Buy_and_Sell_Stock_II b = new Best_Time__to_Buy_and_Sell_Stock_II();
+            int profits = b.MaxProfit(arr);
+            Assert.AreEqual(4, profits);
+        }
+    }
+}

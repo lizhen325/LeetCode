2d5fe2bdfb5acdbb96d150157e0beef783aa1ad5
lizhen325 <zhenli336@gmail.com>
2016-02-17 03:45:12 +1300

    287.Find the Duplicate Number 	new file:   LeetCode/LeetCode/LeetCode/287FindtheDuplicateNumber.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest287_FindtheDuplicatNumber.cs 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/287FindtheDuplicateNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class _287FindtheDuplicateNumber
+    {
+        public static int FindDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]] += 1;
+                else
+                    dic[nums[i]] = 1;
+            }
+            foreach(var item in dic)
+            {
+                if(item.Value > 1)
+                {
+                    return item.Key;
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest287_FindtheDuplicatNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class UnitTest287_FindtheDuplicatNumber
+    {
+        [TestMethod()]
+        public void FindtheDuplicatNumber()
+        {
+            int[] nums = { 2, 2, 2, 3, 4, 5 };
+            int duplicateNumber = _287FindtheDuplicateNumber.FindDuplicate(nums);
+            Assert.AreEqual(2, duplicateNumber);
+        }
+    }
+}

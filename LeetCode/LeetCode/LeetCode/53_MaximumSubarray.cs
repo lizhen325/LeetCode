5c1de5e77c5a690102e518cd6c5b027b0ecc1eed
lizhen325 <zhenli336@gmail.com>
2016-02-17 16:37:27 +1300

    53. Maximum Sub Array 	new file:   LeetCode/LeetCode/LeetCode/53_MaximumSubarray.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/UnitTest53_MaximumSubArray.cs 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/53_MaximumSubarray.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public static class MaximumSubarray
+    {
+        public static int MaxSubArray(int[] nums)
+        {
+            int max_so_far = nums[0];
+            int max_end = nums[0];
+            for(int i=1; i<nums.Length; i++)
+            {
+                max_end = Math.Max(nums[i], max_end + nums[i]);
+                max_so_far = Math.Max(max_so_far, max_end);
+            }
+            return max_so_far;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/UnitTest53_MaximumSubArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class UnitTest53_MaximumSubArray
+    {
+        [TestMethod()]
+        public void MaximumSubArray()
+        {
+            int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            int maximum = MaximumSubarray.MaxSubArray(nums);
+            Assert.AreEqual(6, maximum);
+        }
+    }
+}

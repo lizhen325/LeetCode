090794da814b8d63c35bc618b48c452122bade8d
lizhen325 <zhenli336@gmail.com>
2016-03-07 03:02:56 +1300

    153.Find Minimum in Rotated Sorted Array 	new file:   LeetCode/LeetCode/LeetCode/153_FindMinimumInRotatedSortedArray.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_153_FindMinimumInRotatedSortedArrayTests.cs

---
+++ LeetCode/LeetCode/LeetCode/153_FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _153_FindMinimumInRotatedSortedArray
+    {
+        public static int FindMin(int[] nums)
+        {
+            if (nums.Length == 1)
+                return nums[0];
+            for(int i =1; i < nums.Length; i++)
+            {
+                if(nums[i] < nums[i-1])
+                {
+                    return nums[i];
+                }
+            }
+            return nums[0];
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_153_FindMinimumInRotatedSortedArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _153_FindMinimumInRotatedSortedArrayTests
+    {
+        [TestMethod()]
+        public void FindMinTest()
+        {
+            int[] nums = {4,5,6,7,0,1,2,3 };
+            int res = LeetCode._153_FindMinimumInRotatedSortedArray.FindMin(nums);
+            Assert.AreEqual(0,res);
+        }
+    }
+}

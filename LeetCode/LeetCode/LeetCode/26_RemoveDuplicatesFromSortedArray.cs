7cc258cf1232e816e48f757d0583736d0aa9bbf9
lizhen325 <zhenli336@gmail.com>
2016-03-07 12:04:43 +1300

    26. Remove Duplicates from Sorted Array

---
+++ LeetCode/LeetCode/LeetCode/26_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _26_RemoveDuplicatesFromSortedArray
+    {
+        public static int RemoveDuplicates(int [] nums)
+        {
+            int a = 1;
+            if (nums.Length <= 1) return nums.Length;
+            for(int i=1; i<nums.Length; i++)
+            {
+                if(nums[i] != nums[i-1])
+                {
+                    nums[a] = nums[i];
+                    a++;
+                }
+            }
+            return a;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_26_RemoveDuplicatesFromSortedArrayTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _26_RemoveDuplicatesFromSortedArrayTests
+    {
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            int[] nums = { 1, 1, 2 };
+            int count = LeetCode._26_RemoveDuplicatesFromSortedArray.RemoveDuplicates(nums);
+            Assert.AreEqual(2, count);
+        }
+    }
+}

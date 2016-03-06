d36dbedc6a3e69a19c138710f0f5718261fc30a1
lizhen325 <zhenli336@gmail.com>
2016-03-07 12:36:36 +1300

    66. Plus One 	new file:   LeetCode/LeetCode/LeetCode/66_PlusOne.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_66_PlusOneTests.cs

---
+++ LeetCode/LeetCode/LeetCode/66_PlusOne.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _66_PlusOne
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+            for(int i=digits.Length-1; i>=0; i--)
+            {
+                if(digits[i] == 9)
+                {
+                    digits[i] = 0;
+                }
+                else
+                {
+                    digits[i] += 1;
+                    return digits;
+                }
+                if(digits[0] == 0)
+                {
+                    int[] NewDigits = new int[digits.Length + 1];
+                    NewDigits[0] = 1;
+                    return NewDigits;
+                }
+            }
+            return digits;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_66_PlusOneTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _66_PlusOneTests
+    {
+        [TestMethod()]
+        public void PlusOneTest()
+        {
+            int[] nums = { 8, 9 };
+            int[] res = LeetCode._66_PlusOne.PlusOne(nums);           
+            CollectionAssert.AreEqual(new int[] {9,0},res);
+
+            int [] nums1 = { 9, 9 };
+            int[] res1 = LeetCode._66_PlusOne.PlusOne(nums1);
+            CollectionAssert.AreEqual(new int[] { 1, 0, 0 }, res1);
+        }
+    }
+}

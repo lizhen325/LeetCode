87ce3ae65254a5d72929f2fdff8e7ad13a8cc41e
lizhen325 <zhenli336@gmail.com>
2016-03-08 02:40:27 +1300

    81. Search in Rotated Sorted Array II 	new file:   LeetCode/LeetCode/LeetCode/81_SearchInRotatedSortedArrayII.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_81_SearchInRotatedSortedArrayIITests.cs

---
+++ LeetCode/LeetCode/LeetCode/81_SearchInRotatedSortedArrayII.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _81_SearchInRotatedSortedArrayII
+    {
+        public static bool Search(int[] nums, int target)
+        {
+            Array.Sort(nums);
+            if(nums.Length <= 1)
+            {
+                if (nums[0] == target)
+                    return true;
+                else
+                    return false;
+            }
+            int upper = nums.Length - 1;
+            int lower = 0;
+            int mid;
+            while(lower <= upper)
+            {
+                mid = Convert.ToInt32(lower+(upper-lower) / 2);
+                if (nums[mid] == target)
+                    return true;
+                else
+                {
+                    if (nums[mid] > target)
+                        upper = mid - 1;
+                    else
+                        lower = mid + 1;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_81_SearchInRotatedSortedArrayIITests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _81_SearchInRotatedSortedArrayIITests
+    {
+        [TestMethod()]
+        public void SearchTest()
+        {
+            int[] nums = { 1, 2, 3, 4, 5, 4 };
+            bool flag = LeetCode._81_SearchInRotatedSortedArrayII.Search(nums, 2);
+            Assert.AreEqual(true, flag);
+
+            int[] nums1 = { 1, 2, 3, 4, 5, 9 };
+            bool flag1 = LeetCode._81_SearchInRotatedSortedArrayII.Search(nums1, 8);
+            Assert.AreEqual(false, flag1);
+
+            int[] nums2 = { 1 };
+            bool flag2 = LeetCode._81_SearchInRotatedSortedArrayII.Search(nums2, 2);
+            Assert.AreEqual(false,flag2);
+
+            int[] nums3 = { 1, 1 };
+            bool flag3 = LeetCode._81_SearchInRotatedSortedArrayII.Search(nums3, 2);
+            Assert.AreEqual(false, flag3);
+        }
+    }
+}

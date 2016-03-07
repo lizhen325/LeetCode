cdd6a61020451b205f7edf3751aa8116540912b6
lizhen325 <zhenli336@gmail.com>
2016-03-07 13:41:33 +1300

    162. Find Peak Element 	new file:   LeetCode/LeetCode/LeetCode/162_FindPeakElement.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_162_FindPeakElementTests.cs

---
+++ LeetCode/LeetCode/LeetCode/162_FindPeakElement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _162_FindPeakElement
+    {
+        public static int FindPeakElement(int[] nums)
+        {
+            if(nums.Length<=1)
+            {
+                return nums.Length - 1;
+            }
+            for(int i=1; i<nums.Length; i++)
+            {
+                if(i < nums.Length-1)
+                {
+                    if (nums[i] > nums[i - 1] && nums[i + 1] < nums[i])
+                    {
+                        return i;
+                    }
+                }
+                if (nums[i] < nums[i - 1])
+                {
+                    return i - 1;
+                }
+   
+                if(i > nums.Length-2)
+                {
+                    return nums.Length - 1;
+                }                            
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_162_FindPeakElementTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _162_FindPeakElementTests
+    {
+        [TestMethod()]
+        public void FindPeakElementTest()
+        {
+            int [] nums = { 1, 2, 3, 1 };
+            int res = LeetCode._162_FindPeakElement.FindPeakElement(nums);
+            Assert.AreEqual(2, res);
+
+            int[] nums1 = { 1, 2, 3, 4, 5 };
+            int res1 = LeetCode._162_FindPeakElement.FindPeakElement(nums1);
+            Assert.AreEqual(4, res1);
+
+            int[] nums2 = { 2, 1 };
+            int res2 = LeetCode._162_FindPeakElement.FindPeakElement(nums2);
+            Assert.AreEqual(0, res2);
+
+            int[] nums3 = { 2 };
+            int res3 = LeetCode._162_FindPeakElement.FindPeakElement(nums3);
+            Assert.AreEqual(0, res3);
+        }
+    }
+}

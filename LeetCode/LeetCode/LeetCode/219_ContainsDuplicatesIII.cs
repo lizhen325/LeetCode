ac8fb01285f2f1778a35030048ca5719c23842c0
lizhen325 <zhenli336@gmail.com>
2016-03-07 16:37:37 +1300

    219. Contains Duplicate II 	new file:   LeetCode/LeetCode/LeetCode/219_ContainsDuplicatesIII.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_219_ContainsDuplicatesIIITests.cs

---
+++ LeetCode/LeetCode/LeetCode/219_ContainsDuplicatesIII.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _219_ContainsDuplicatesIII
+    {
+        public static bool ContainNearByDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                {
+                    if (i - dic[nums[i]] <= k)
+                        return true;
+                    else
+                        dic[nums[i]] = i;
+                }
+                else
+                    dic.Add(nums[i], i);
+            }
+            return false;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_219_ContainsDuplicatesIIITests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _219_ContainsDuplicatesIIITests
+    {
+        [TestMethod()]
+        public void ContainNearByDuplicateTest()
+        {
+            int[] nums = { 2, 3, 1, 4, 2 };
+            bool res = LeetCode._219_ContainsDuplicatesIII.ContainNearByDuplicate(nums, 4);
+            Assert.IsTrue(res);
+        }
+    }
+}

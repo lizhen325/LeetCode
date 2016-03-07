d50fc21390fd51919aa1b72337004f7c27fe0fde
lizhen325 <zhenli336@gmail.com>
2016-03-07 15:14:51 +1300

    80. Remove Duplicates from Sorted Array II

---
+++ LeetCode/LeetCode/LeetCode/80_RemoveDuplicatesFormSortedArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _80_RemoveDuplicatesFormSortedArray
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            int current = 0;
+            int i;
+            int j = 0;
+            for (i=0; i < nums.Length; )
+            {
+                int now = nums[i];
+                for (j = i; j < nums.Length; j++)
+                {
+                    if (nums[j] != now)
+                        break;
+                    if (j - i < 2)
+                        nums[current++] = now;
+                }
+                i = j;
+            }
+            return current;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_80_RemoveDuplicatesFormSortedArrayTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _80_RemoveDuplicatesFormSortedArrayTests
+    {
+        [TestMethod()]
+        public void RemoveDuplicatesTest()
+        {
+            int[] nums = { 1, 1, 1, 2, 2, 2, 3 };
+            int res = LeetCode._80_RemoveDuplicatesFormSortedArray.RemoveDuplicates(nums);
+            Assert.AreEqual(5, res);
+
+            int[] nums1 = { 1, 1, 1, 1 };
+            int res1 = LeetCode._80_RemoveDuplicatesFormSortedArray.RemoveDuplicates(nums1);
+            Assert.AreEqual(2, res1);
+
+            int[] nums2 = { 1, 1, 1, 2 };
+            int res2 = LeetCode._80_RemoveDuplicatesFormSortedArray.RemoveDuplicates(nums2);
+            Assert.AreEqual(3, res2);
+        }
+    }
+}

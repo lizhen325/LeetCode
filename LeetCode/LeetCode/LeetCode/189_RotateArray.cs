d5f5f9f9a268c8aaf5ca6e96bd050432b1477dd3
lizhen325 <zhenli336@gmail.com>
2016-03-07 15:56:00 +1300

    189. Rotate Array 	new file:   LeetCode/LeetCode/LeetCode/189_RotateArray.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj

---
+++ LeetCode/LeetCode/LeetCode/189_RotateArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _189_RotateArray
+    {
+        public static void Rotate(int[] nums,int k)
+        {
+            k = k % nums.Length;
+            Array.Reverse(nums);
+            Array.Reverse(nums, 0, k);
+            Array.Reverse(nums, k, nums.Length - k);
+        }
+    }
+}

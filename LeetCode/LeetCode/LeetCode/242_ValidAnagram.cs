53b8391b3917424a286894fc0cdd8de071f7ac02
lizhen325 <zhenli336@gmail.com>
2016-03-10 13:40:10 +1300

    242. Valid Anagram 	new file:   LeetCode/LeetCode/LeetCode/242_ValidAnagram.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_242_ValidAnagramTests.cs

---
+++ LeetCode/LeetCode/LeetCode/242_ValidAnagram.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _242_ValidAnagram
+    {
+        public static bool IsAnagram(string s, string t)
+        {
+            if(s.Length != t.Length) return false;
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+            for(int i=0; i<s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                    dic[s[i]]++;
+                else
+                    dic[s[i]] = 1;
+            }
+
+           for(int i=0; i<t.Length; i++)
+           {
+               if (!dic.ContainsKey(t[i]) || dic[t[i]] == 0)
+                   return false;
+               else
+                   dic[t[i]]--;
+           }
+           return true;
+
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_242_ValidAnagramTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _242_ValidAnagramTests
+    {
+        [TestMethod()]
+        public void IsAnagramTest()
+        {
+            string s1 = "anagram";
+            string t1 = "nagaram";
+            bool flag1 = LeetCode._242_ValidAnagram.IsAnagram(s1, t1);
+            Assert.AreEqual(true, flag1);
+
+            string s2 = "rat";
+            string t2 = "car";
+            bool flag2 = LeetCode._242_ValidAnagram.IsAnagram(s2, t2);
+            Assert.AreEqual(false, flag2);
+        }
+    }
+}

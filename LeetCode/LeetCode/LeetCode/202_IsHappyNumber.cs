102bb4f3c181cc546b538af5945be4c29a23b1bc
lizhen325 <zhenli336@gmail.com>
2016-03-10 14:56:59 +1300

    202. Happy Number 	new file:   LeetCode/LeetCode/LeetCode/202_IsHappyNumber.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_202_IsHappyNumberTests.cs

---
+++ LeetCode/LeetCode/LeetCode/202_IsHappyNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _202_IsHappyNumber
+    {
+        public static bool IsHappy(int n)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            while(true)
+            {
+                if (n == 1) return true;
+                if (dic.ContainsKey(n)) return false;
+                dic[n] = "";
+                n = Sum(n);
+            }
+        }
+
+        public static int Sum(int n)
+        {
+            string s = n.ToString();
+            int sum = 0;
+            for(int i=0; i<s.Length; i++)
+            {
+                sum += Convert.ToInt32(s[i].ToString()) * Convert.ToInt32(s[i].ToString());
+            }
+            return sum;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_202_IsHappyNumberTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _202_IsHappyNumberTests
+    {
+        [TestMethod()]
+        public void IsHappyTest()
+        {
+            bool flag1 = LeetCode._202_IsHappyNumber.IsHappy(19);
+            Assert.AreEqual(true, flag1);
+        }
+    }
+}

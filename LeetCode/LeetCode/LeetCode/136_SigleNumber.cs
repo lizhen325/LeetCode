019ad4872c9c9f0b55f1c69414b92490a3b0711b
lizhen325 <zhenli336@gmail.com>
2016-03-10 13:26:07 +1300

    136. Single Number 	new file:   LeetCode/LeetCode/LeetCode/136_SigleNumber.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_146_SigleNumberTests.cs 	deleted:    LeetCode/LeetCode/UnitTestLeetCode/_41_FirstMissingPositiveTests.cs

---
+++ LeetCode/LeetCode/LeetCode/136_SigleNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _136_SigleNumber
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for(int i=0; i<nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]]++;
+                else
+                    dic[nums[i]] = 1;
+            }
+
+            foreach(KeyValuePair<int,int> i in dic)
+            {
+                if (i.Value == 1)
+                    return i.Key;
+            }
+            return 0;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_146_SigleNumberTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _136_SigleNumberTests
+    {
+        [TestMethod()]
+        public void SingleNumberTest()
+        {
+            int[] nums1 = { 1, 2, 3, 4, 1,2,4 };
+            int res1 = LeetCode._136_SigleNumber.SingleNumber(nums1);
+            Assert.AreEqual(3, res1);
+        }
+    }
+}

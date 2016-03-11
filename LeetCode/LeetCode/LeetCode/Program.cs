faebe482f7d027b5b9c1d0080f285247ec75f39d
lizhen325 <zhenli336@gmail.com>
2016-03-11 20:48:25 +1300

    LinQ Move Zeroes 	modified:   LeetCode/LeetCode/LeetCode.sln 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/LeetCode/Program.cs 	new file:   LeetCode/LeetCode/LeetCodeLinQ/LeetCodeLinQ.csproj 	new file:   LeetCode/LeetCode/LeetCodeLinQ/Properties/AssemblyInfo.cs 	new file:   LeetCode/LeetCode/LeetCodeLinQ/RemoveZeroes.cs

--- LeetCode/LeetCode/LeetCode/Program.cs
+++ LeetCode/LeetCode/LeetCode/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LeetCodeLinQ;
 
 namespace LeetCode
 {
@@ -10,7 +11,11 @@ namespace LeetCode
     {
         public static void Main()
         {
-
+            int[] nums = { 0, 1, 0, 3, 12 };
+            int[] res = RemoveZeroes.MoveZeroes(nums);
+            foreach(var i in res)
+                Console.WriteLine(i);
+            Console.ReadKey();
         }
     }
 }
---
+++ LeetCode/LeetCode/LeetCodeLinQ/RemoveZeroes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeLinQ
+{
+    public class RemoveZeroes
+    {
+        public static int[] MoveZeroes(int[] nums)
+        {
+            IEnumerable<int> zeroes = from ze in nums
+                                      where ze == 0
+                                      select ze;
+            IEnumerable<int> non_zeroes = from non in nums
+                                          where non != 0
+                                          select non;
+            int[] res = non_zeroes.Concat(zeroes).ToArray();
+            return res;
+        }
+    }
+}

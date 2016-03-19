1948d2207c8fe7c64e20d2cce9d14736230d6573
lizhen325 <zhenli336@gmail.com>
2016-03-20 11:35:27 +1300

    290. WordPatternTests 	new file:   LeetCode/LeetCode/LeetCode/290_WordPattern.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_290_WordPatternTests.cs

---
+++ LeetCode/LeetCode/LeetCode/290_WordPattern.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _290_WordPattern
+    {
+        public static bool WordPattern(string pattern, string str)
+        {
+
+            // pattern = "abba", str = "dog cat cat dog" should return true.
+            // pattern = "abba", str = "dog cat cat fish" should return false.
+            // pattern = "aaaa", str = "dog cat cat dog" should return false.
+            // pattern = "abba", str = "dog dog dog dog" should return false.
+            Dictionary<char, string> dic = new Dictionary<char, string>();
+            Dictionary<string, char> dic2 = new Dictionary<string, char>();
+            string[] words = str.Split(' ');
+            //string[] words = str.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+            if (pattern.Length != words.Length) return false;
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (!dic.ContainsKey(pattern[i]) && !dic2.ContainsKey(words[i]))
+                {
+                    dic.Add(pattern[i], words[i]);
+                    dic2.Add(words[i], pattern[i]);
+                }
+                else if (dic.ContainsKey(pattern[i]) && dic2.ContainsKey(words[i])
+               && dic[pattern[i]] == words[i] && dic2[words[i]] == pattern[i])
+                {
+                    continue;
+                }
+                else
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_290_WordPatternTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _290_WordPatternTests
+    {
+        [TestMethod()]
+        public void WordPatternTest()
+        {
+            string pattern = "abba";
+            string words = "dog cat cat dog";
+            bool res = LeetCode._290_WordPattern.WordPattern(pattern, words);
+            Assert.AreEqual(true, res);
+
+            string pattern1 = "abba";
+            string words1 = "dog cat, cat, fish";
+            bool res1 = LeetCode._290_WordPattern.WordPattern(pattern1, words1);
+            Assert.AreEqual(false, res1);
+
+            string pattern2 = "abba";
+            string words2 = "dog dog dog dog";
+            bool res2 = LeetCode._290_WordPattern.WordPattern(pattern2, words2);
+            Assert.AreEqual(false, res2);
+        }
+    }
+}

55c11920c9179f5ac580c65f57361b51e975d6ec
lizhen325 <zhenli336@gmail.com>
2016-03-21 02:58:31 +1300

    3. Lognest Substring 	new file:   LeetCode/LeetCode/LeetCode/3_LongestSubstring.cs 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_3_LongestSubstringTests.cs

---
+++ LeetCode/LeetCode/LeetCode/3_LongestSubstring.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _3_LongestSubstring
+    {
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int length = 0;
+            for (int start = 0; start < s.Length; start++)
+            {
+                int i = start;
+                var dic = new Dictionary<char, int>();
+                while (i < s.Length && !dic.ContainsKey(s[i])) 
+                { 
+                    dic.Add(s[i++], 1); 
+                } 
+                if (i - start > length)
+                {
+                    length = i - start;
+                }
+            }
+
+            return length;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_3_LongestSubstringTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _3_LongestSubstringTests
+    {
+        [TestMethod()]
+        public void LengthOfLongestSubstringTest()
+        {
+            string str1 = "abcabccbb";
+            int res = LeetCode._3_LongestSubstring.LengthOfLongestSubstring(str1);
+            Assert.AreEqual(3, res);
+
+            string str2 = "bbbbb";
+            int res2 = LeetCode._3_LongestSubstring.LengthOfLongestSubstring(str2);
+            Assert.AreEqual(1, res2);
+
+            string str3 = "pwwkew";
+            int res3 = LeetCode._3_LongestSubstring.LengthOfLongestSubstring(str3);
+            Assert.AreEqual(3, res3);
+        }
+    }
+}

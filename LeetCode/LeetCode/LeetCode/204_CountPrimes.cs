5e44928890438777725ba5e1319eb36138590ba1
lizhen325 <zhenli336@gmail.com>
2016-03-20 16:00:35 +1300

    204.Count Primes 	new file:   LeetCode/LeetCode/LeetCode/204_CountPrimes.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_204_CountPrimesTests.cs

---
+++ LeetCode/LeetCode/LeetCode/204_CountPrimes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _204_CountPrimes
+    {
+        public static int CountPrimes(int n)
+        {
+            int count = 0;
+            for(int i=1; i<n; i++)
+            {
+                if (isPrime(i))
+                    count++;
+            }
+            return count;
+        }
+
+        private static bool isPrime(int num)
+        {
+            if (num <= 1) return false;
+            int up = (int)Math.Pow(num, 0.5);
+            for(int i=2; i<=up; i++)
+            {
+                if (num % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_204_CountPrimesTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _204_CountPrimesTests
+    {
+        [TestMethod()]
+        public void CountPrimesTest()
+        {
+            int n = 5;
+            int res = LeetCode._204_CountPrimes.CountPrimes(n);
+            Assert.AreEqual(2, res);
+        }
+    }
+}

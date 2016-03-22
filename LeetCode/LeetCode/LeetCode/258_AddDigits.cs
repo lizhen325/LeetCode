8cc3cd1fcea6641bcef5b919e1824fa4d2dfe285
lizhen325 <zhenli336@gmail.com>
2016-03-23 01:13:22 +1300

    258. Add Digits 	new file:   LeetCode/LeetCode/LeetCode/258_AddDigits.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_258_AddDigitsTests.cs

---
+++ LeetCode/LeetCode/LeetCode/258_AddDigits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _258_AddDigits
+    {
+        public static int AddDigits(int num)
+        {
+            return num - 9 * ((num - 1) / 9);
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_258_AddDigitsTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _258_AddDigitsTests
+    {
+        [TestMethod()]
+        public void AddDigitsTest()
+        {
+            int num = 38;
+            int res = LeetCode._258_AddDigits.AddDigits(num);
+            Assert.AreEqual(2, res);
+        }
+    }
+}

2f5517ca276fc65dd58479bb6be2e5cf2c4d0268
lizhen325 <zhenli336@gmail.com>
2016-03-24 13:19:43 +1300

    11. Container With Most Water 	new file:   LeetCode/LeetCode/LeetCode/11_ContianerWithMostWater.cs 	modified:   LeetCode/LeetCode/LeetCode/LeetCode.csproj 	modified:   LeetCode/LeetCode/UnitTestLeetCode/UnitTestLeetCode.csproj 	new file:   LeetCode/LeetCode/UnitTestLeetCode/_11_ContianerWithMostWaterTests.cs

---
+++ LeetCode/LeetCode/LeetCode/11_ContianerWithMostWater.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _11_ContianerWithMostWater
+    {
+        public static int MaxArea(int[] height)
+        {
+            int i, j, lh, rh, area, temp;
+            i = 0;
+            j = height.Length - 1;
+            area = 0;
+            lh = height[0];
+            rh = height[height.Length - 1];
+            while(i<j)
+            {
+                temp = Math.Min(lh, rh) * (j - i);
+                if (temp > area) area = temp;
+                if(lh < rh)
+                {
+                    while(i<j && height[i] <=lh)
+                    {
+                        i++;
+                    }
+                    if (i < j) lh = height[i];
+                }
+                else
+                {
+                    while(i<j && height[j] <=rh)
+                    {
+                        j--;
+                    }
+                    if (i < j) rh = height[j];
+                }
+            }
+            return area;
+        }
+    }
+}
---
+++ LeetCode/LeetCode/UnitTestLeetCode/_11_ContianerWithMostWaterTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeetCode;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace LeetCode.Tests
+{
+    [TestClass()]
+    public class _11_ContianerWithMostWaterTests
+    {
+        [TestMethod()]
+        public void MaxAreaTest()
+        {
+            int[] height1 = {1,2,3,4,5,6,6};
+            int MaxArea = LeetCode._11_ContianerWithMostWater.MaxArea(height1);
+            Assert.AreEqual(5, MaxArea);
+        }
+    }
+}

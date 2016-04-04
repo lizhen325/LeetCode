d800a6c32a43c050936c48dbb39c8c4c97880044
lizhen325 <zhenli336@gmail.com>
2016-04-04 13:06:13 +1200

    110. Balanced Binary Tree

---
+++ LeetCode/LeetCode/LeetCode/110 Balanced Binary Tree.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _110_Balanced_Binary_Tree
+    {
+        public bool IsBalanced(TreeNode root)
+        {
+            int leftNode = 0;
+            int rightNode = 0;
+
+        }
+
+        private int GetHeight(TreeNode root)
+        {
+            int leftNode = GetHeight(root.left);
+            int rightNode = GetHeight(root.right);
+            return Math.Abs(leftNode - rightNode);
+        }
+    }
+}

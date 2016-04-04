4215fa5f61cd64de2e07019a7248f27666220cc5
lizhen325 <zhenli336@gmail.com>
2016-04-04 13:44:03 +1200

    Inverted Tree

---
+++ LeetCode/LeetCode/LeetCode/226 Inverted Tree.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _226_Inverted_Tree
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null) return null;
+            var left = root.left;
+            var right = root.right;
+            root.right = this.InvertTree(left);
+            root.left = this.InvertTree(right);
+
+            return root;
+        }
+    }
+}
